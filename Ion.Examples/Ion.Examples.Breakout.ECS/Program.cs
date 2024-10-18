b6fbe56984579e304e8e5e4dbb7615a1c2713cc2
Jim Buck <jim@jimbuck.io>
2024-10-18 16:04:35 -0400

    PhysX integration.

--- Ion.Examples/Ion.Examples.Breakout.ECS/Program.cs
+++ Ion.Examples/Ion.Examples.Breakout.ECS/Program.cs
@@ -2,11 +2,14 @@
 
 using Arch.Core;
 using Arch.Core.Extensions;
+using MagicPhysX;
+using MagicPhysX.Toolkit;
 
 using Ion;
 using Ion.Extensions.Assets;
 using Ion.Extensions.Graphics;
 using Ion.Extensions.Audio;
+
 
 var builder = IonApplication.CreateBuilder(args);
 
@@ -16,35 +19,40 @@ builder.Services.AddIon(builder.Configuration, graphics =>
 	graphics.ClearColor = new Color(0x333);
 });
 
-builder.Services.AddSingleton(services => World.Create());
 builder.Services.AddSingleton<MouseCaptureSystem>();
 builder.Services.AddSingleton<SpriteRendererSystem>();
-builder.Services.AddSingleton<ScoreSystem>();
-builder.Services.AddSingleton<SoundEffectsSystem>();
-builder.Services.AddSingleton<PaddleSystem>();
-builder.Services.AddSingleton<BallSystem>();
-builder.Services.AddSingleton<BlockSystem>();
+builder.Services.AddScoped(services => World.Create());
+builder.Services.AddScoped<ScoreSystem>();
+builder.Services.AddScoped<SoundEffectsSystem>();
+builder.Services.AddScoped<PaddleSystem>();
+builder.Services.AddScoped<BallSystem>();
+builder.Services.AddScoped<BlockSystem>();
+builder.Services.AddScoped(services => new PhysicsSystem(true));
+builder.Services.AddScoped(services => services.GetRequiredService<PhysicsSystem>().CreateScene(Vector3.Zero));
+builder.Services.AddScoped<BreakoutPhysicsSystem>();
 
 var game = builder.Build();
 game.UseIon()
 	.UseSystem<MouseCaptureSystem>()
-	.UseSystem<SpriteRendererSystem>()
-	.UseSystem<ScoreSystem>()
+	.UseSystem<BreakoutPhysicsSystem>()
 	//.UseSystem<SoundEffectsSystem>()
 	.UseSystem<PaddleSystem>()
 	.UseSystem<BallSystem>()
 	.UseSystem<BlockSystem>()
+	.UseSystem<SpriteRendererSystem>()
+	.UseSystem<ScoreSystem>()
 	;
 
 //Thread.Sleep(10 * 1000); // Delay to let diagnostics warm up.
 
 game.Run();
 
-
+public record struct DynamicRigidBody(Rigidbody Body);
+public record struct KinematicRigidBody(Rigidbody Body);
+public record struct StaticBody(Rigidstatic Body);
+public record struct Sprite(Texture2D Texture, Vector2 Size);
 public record struct Position(Vector2 Value);
 public record struct GridLocation(int Row, int Column);
-public record struct Velocity(Vector2 Direction, float Speed);
-public record struct Sprite(Texture2D Texture, Vector2 Size);
 public record struct Paddle(bool HasBall);
 public record struct Ball();
 
@@ -78,9 +86,6 @@ public static class BreakoutConstants
 
 	public static readonly Vector2 BALL_SIZE = new(32f, 32f);
 	public static readonly float INITIAL_BALL_SPEED = 400f;
-
-	public static readonly Vector2 PADDLE_BOUNCE_MIN = Vector2.Normalize(new Vector2(-1, -0.75f));
-	public static readonly Vector2 PADDLE_BOUNCE_MAX = Vector2.Normalize(new Vector2(+1, -0.75f));
 }
 
 public class MouseCaptureSystem(IWindow window, IInputState input)
@@ -123,7 +128,51 @@ public class SpriteRendererSystem(ISpriteBatch spriteBatch, World world)
 	public void Render(GameTime dt, GameLoopDelegate next)
 	{
 		world.Query(in _spriteQuery, (Entity entity, ref Sprite sprite, ref Position position) => {
-			spriteBatch.Draw(sprite.Texture, new RectangleF(position.Value, sprite.Size));
+			spriteBatch.Draw(sprite.Texture, new RectangleF(position.Value - (sprite.Size / 2), sprite.Size));
+		});
+
+		next(dt);
+	}
+}
+
+public class BreakoutPhysicsSystem(IWindow window, World world, PhysicsScene physicsScene)
+{
+	private readonly QueryDescription _kinematicQuery = new QueryDescription().WithAll<KinematicRigidBody, Position>();
+	private readonly QueryDescription _dynamicQuery = new QueryDescription().WithAll<DynamicRigidBody, Position>();
+
+	[Init]
+	public unsafe void Init(GameTime dt, GameLoopDelegate next)
+	{
+		var wallMaterial = physicsScene.CreateMaterial(0.5f, 0.5f, 1.1f);
+		var sideWallHalfExtent = new Vector3(5, window.Height / 2, 5);
+		var leftWallPosition = new Vector3(-5, window.Height / 2, 0);
+		var rightWallPosition = new Vector3(window.Width + 5, window.Height / 2, 0);
+
+		var topWallHalfExtent = new Vector3(window.Width / 2, 5, 5);
+		var topWallPosition = new Vector3(window.Width / 2, 0, 0);
+
+		var leftWall = physicsScene.AddStaticBox(sideWallHalfExtent, leftWallPosition, Quaternion.Identity, wallMaterial);
+		var rightWall = physicsScene.AddStaticBox(sideWallHalfExtent, rightWallPosition, Quaternion.Identity, wallMaterial);
+		var topWall = physicsScene.AddStaticBox(topWallHalfExtent, topWallPosition, Quaternion.Identity, wallMaterial);
+
+		next(dt);
+	}
+
+	[FixedUpdate]
+	public void Update(GameTime dt, GameLoopDelegate next)
+	{
+		world.Query(in _kinematicQuery, (ref KinematicRigidBody rigidBody, ref Position position) =>
+		{
+			var position3d = new Vector3(position.Value, 0);
+			rigidBody.Body.position = position3d;
+		});
+
+		physicsScene.Update(dt.Delta);
+
+		world.Query(in _dynamicQuery, (ref DynamicRigidBody rigidBody, ref Position position) =>
+		{
+			var position3d = rigidBody.Body.transform.position;
+			position.Value = new Vector2(position3d.X, position3d.Y);
 		});
 
 		next(dt);
@@ -189,16 +238,22 @@ public class SoundEffectsSystem(IAssetManager assets, IEventListener events, IAu
 	}
 }
 
-public class PaddleSystem(IWindow window, World world, IInputState input, IEventListener events, IAssetManager assets)
+public class PaddleSystem(IWindow window, World world, IInputState input, IEventListener events, IAssetManager assets, PhysicsScene physicsScene)
 {
-	private EntityReference _paddle = default!;
+	private EntityReference _paddle = EntityReference.Null;
+	private Rigidbody _paddleBody = default!;
 
 	[Init]
-	public void Init(GameTime dt, GameLoopDelegate next)
+	public unsafe void Init(GameTime dt, GameLoopDelegate next)
 	{
 		var paddleTexture = assets.Load<Texture2D>("49-Breakout-Tiles.png");
-		var paddleLocation = new Vector2(Math.Clamp(input.MousePosition.X - (BreakoutConstants.PADDLE_SIZE.Y / 2f), 0, window.Width - BreakoutConstants.PADDLE_SIZE.X), window.Size.Y - (BreakoutConstants.BLOCK_SIZE.Y + BreakoutConstants.BOTTOM_GAP));
-		_paddle = world.Create(new Paddle(true), new Position(paddleLocation), new Sprite(paddleTexture, BreakoutConstants.PADDLE_SIZE)).Reference();
+		var paddlePosition = new Vector2(window.Width / 2f, window.Size.Y - (BreakoutConstants.BLOCK_SIZE.Y + BreakoutConstants.BOTTOM_GAP));
+		
+		var material = physicsScene.CreateMaterial(0.5f, 0.5f, 1.2f);
+
+		var paddlePosition3d = new Vector3(paddlePosition, 0);
+		_paddleBody = physicsScene.AddKinematicCapsule(BreakoutConstants.PADDLE_SIZE.Y / 2f, (BreakoutConstants.PADDLE_SIZE.X - BreakoutConstants.PADDLE_SIZE.Y) / 2f, paddlePosition3d, Quaternion.Identity, 1, material);
+		_paddle = world.Create(new Paddle(true), new Position(paddlePosition), new Sprite(paddleTexture, BreakoutConstants.PADDLE_SIZE), new KinematicRigidBody(_paddleBody)).Reference();
 
 		next(dt);
 	}
@@ -211,7 +266,7 @@ public class PaddleSystem(IWindow window, World world, IInputState input, IEvent
 			ref var paddleComponent = ref _paddle.Entity.Get<Paddle>();
 			ref var paddlePosition = ref _paddle.Entity.Get<Position>();
 
-			paddlePosition.Value = new Vector2(input.MousePosition.X - (BreakoutConstants.PADDLE_SIZE.X / 2), paddlePosition.Value.Y);
+			paddlePosition.Value = new Vector2(input.MousePosition.X, paddlePosition.Value.Y);
 
 			if (paddleComponent.HasBall && input.Pressed(MouseButton.Left))
 			{
@@ -223,23 +278,32 @@ public class PaddleSystem(IWindow window, World world, IInputState input, IEvent
 	}
 }
 
-public class BallSystem(World world, IEventListener events, IWindow window, IAssetManager assets)
+public unsafe class BallSystem(IWindow window, World world, IEventListener events, IAssetManager assets, PhysicsScene physicsScene)
 {	
-	private EntityReference _ball = default!;
-	private EntityReference _paddle = default!;
-	private QueryDescription _paddleQuery = new QueryDescription().WithAll<Paddle, Position>();
-	private readonly QueryDescription _blockQuery = new QueryDescription().WithAll<GridLocation, Sprite, Position>();
+	private Texture2D _ballTexture = default!;
+	private PxMaterial* _ballMaterial = default!;
+	private EntityReference _ball = EntityReference.Null;
+	private EntityReference _paddle = EntityReference.Null;
 
-	private readonly Vector2 _paddleBallOffset = new((BreakoutConstants.PADDLE_SIZE.X - BreakoutConstants.BALL_SIZE.X) / 2f, -(BreakoutConstants.BALL_SIZE.Y + 1));
+	private QueryDescription _ballQuery = new QueryDescription().WithAll<Ball>();
+	private QueryDescription _paddleQuery = new QueryDescription().WithAll<Paddle>();
+
+	private readonly Vector2 _paddleBallOffset = new(0, -((BreakoutConstants.BALL_SIZE.Y + BreakoutConstants.PADDLE_SIZE.Y) / 2) - 1);
 
 	[Init]
-	public void Init(GameTime dt, GameLoopDelegate next)
+	public unsafe void Init(GameTime dt, GameLoopDelegate next)
 	{
-		var ballTexture = assets.Load<Texture2D>("58-Breakout-Tiles.png");
+		_ballTexture = assets.Load<Texture2D>("58-Breakout-Tiles.png");
+		_ballMaterial = physicsScene.CreateMaterial(0.5f, 0.5f, 1f);
 
-		_ball = world.Create(new Ball(), new Velocity(Vector2.UnitY, 0), new Position(Vector2.Zero), new Sprite(ballTexture, BreakoutConstants.BALL_SIZE)).Reference();
+		_createBall(new Vector2(window.Width / 2, window.Size.Y - (BreakoutConstants.BLOCK_SIZE.Y + BreakoutConstants.BOTTOM_GAP)));
 
 		next(dt);
+
+		world.Query(in _ballQuery, (Entity entity) =>
+		{
+			_ball = entity.Reference();
+		});
 
 		world.Query(in _paddleQuery, (Entity entity) =>
 		{
@@ -251,169 +315,51 @@ public class BallSystem(World world, IEventListener events, IWindow window, IAss
 	public void PositionUpdate(GameTime dt, GameLoopDelegate next)
 	{
 		ref var ballPosition = ref _ball.Entity.Get<Position>();
-		ref var ballVelocity = ref _ball.Entity.Get<Velocity>();
 		ref var paddle = ref _paddle.Entity.Get<Paddle>();
 
 		if (paddle.HasBall)
 		{
-			ballPosition.Value = _paddle.Entity.Get<Position>().Value + _paddleBallOffset;
+			var paddlePosition = _paddle.Entity.Get<Position>().Value;
+			ballPosition.Value = paddlePosition + _paddleBallOffset;
 
 			if (events.OnLatest<LaunchBallCommand>())
 			{
-				ballVelocity.Direction = -Vector2.UnitY;
-				ballVelocity.Speed = BreakoutConstants.INITIAL_BALL_SPEED;
+				var ballPosition3d = new Vector3(ballPosition.Value, 0);
+				var ballBody = physicsScene.AddDynamicSphere(BreakoutConstants.BALL_SIZE.X / 2f, ballPosition3d, Quaternion.Identity, 1, _ballMaterial);
+				ballBody.constraints = RigidbodyConstraints.FreezePositionZ;
+				_ball.Entity.Add(new DynamicRigidBody(ballBody));
+				ballBody.AddForce(new Vector3(100f, -300, 0), ForceMode.VelocityChange);
+
 				paddle.HasBall = false;
 			}
 		}
-		else
-		{
-			ballPosition.Value += ballVelocity.Direction * ballVelocity.Speed * dt.Delta;
-		}
-
-		_handleWallHits(ref ballPosition, ref ballVelocity, ref paddle);
-		_handlePaddleAndBlockHits(ref ballPosition, ref ballVelocity);
 
 		next(dt);
-	}	
-
-	private void _handleWallHits(ref Position ballPosition, ref Velocity ballVelocity, ref Paddle paddle)
-	{
-		CollisionDirection? wallHitDirection = null;
-		if (ballPosition.Value.X < 0)
-		{
-			ballPosition.Value = new Vector2(0, ballPosition.Value.Y);
-			ballVelocity.Direction *= new Vector2(-1, 1);
-			wallHitDirection = CollisionDirection.Left;
-		}
-
-		if (ballPosition.Value.X >= window.Width - BreakoutConstants.BALL_SIZE.X)
-		{
-			ballPosition.Value = new Vector2(window.Width - (BreakoutConstants.BALL_SIZE.X + 1), ballPosition.Value.Y);
-			ballVelocity.Direction *= new Vector2(-1, 1);
-			wallHitDirection = CollisionDirection.Right;
-		}
-
-		if (ballPosition.Value.Y <= 0)
-		{
-			ballPosition.Value = new Vector2(ballPosition.Value.X, 1);
-			ballVelocity.Direction *= new Vector2(1, -1);
-			wallHitDirection = CollisionDirection.Top;
-		}
-
-		if (ballPosition.Value.Y > window.Height)
-		{
-			ballVelocity.Direction = Vector2.UnitY;
-			ballVelocity.Speed = BreakoutConstants.INITIAL_BALL_SPEED;
-			paddle.HasBall = true;
-			events.Emit(new BallLostEvent());
-		}
-
-		if (wallHitDirection is not null) {
-			ballVelocity.Speed += 5f;
-			events.Emit(new WallHitEvent(wallHitDirection.Value));
-		}
 	}
 
-	private void _handlePaddleAndBlockHits(ref Position ballPosition, ref Velocity ballVelocity)
+	private Entity _createBall(Vector2 position)
 	{
-		ref var paddlePosition = ref _paddle.Entity.Get<Position>();
-		var paddleRect = new RectangleF(paddlePosition.Value, BreakoutConstants.PADDLE_SIZE);
-		var ballRect = new RectangleF(ballPosition.Value, BreakoutConstants.BALL_SIZE);
-
-		RectangleF.Intersect(ref ballRect, ref paddleRect, out var paddleIntersection);
-
-		if (!paddleIntersection.IsEmpty)
-		{
-			ballVelocity.Speed += 5f;
-			var direction = _getIntersectionDirection(ref paddleIntersection, ref ballRect);
-			if (direction == CollisionDirection.Bottom)
-			{
-				var hitSide = (ballRect.X - (paddleRect.X - ballRect.Width)) / (ballRect.Width + paddleRect.Width);
-				ballVelocity.Direction = Vector2.Normalize(Vector2.Lerp(BreakoutConstants.PADDLE_BOUNCE_MIN, BreakoutConstants.PADDLE_BOUNCE_MAX, hitSide));
-				ballPosition.Value = new Vector2(ballPosition.Value.X, paddleRect.Top - (ballRect.Height + 1));
-			}
-			else
-			{
-				ballVelocity.Direction = _recalculateBallDirection(ballVelocity, direction);
-				ballVelocity.Direction = new Vector2(ballVelocity.Direction.X, -MathF.Abs(ballVelocity.Direction.Y));
-				ballPosition.Value = new Vector2(direction == CollisionDirection.Left ? paddleRect.Right + 1 : paddleRect.Left - (ballRect.Width + 1), ballPosition.Value.Y - 1);
-			}
-			events.Emit(new PaddleHitEvent((paddleIntersection.Left - paddleRect.Left) / paddleRect.Width));
-		}
-
-		var collisions = new List<CollisionDirection>();
-		world.Query(in _blockQuery, (Entity entity, ref GridLocation gridLocation, ref Sprite sprite, ref Position position) =>
-		{
-			var i = (gridLocation.Row * BreakoutConstants.COLS) + gridLocation.Column;
-			var blockRect = new RectangleF(position.Value, BreakoutConstants.BLOCK_SIZE);
-
-			RectangleF.Intersect(ref ballRect, ref blockRect, out var blockIntersection);
-			if (blockIntersection.IsEmpty) return;
-
-			var direction = _getIntersectionDirection(ref blockIntersection, ref ballRect);
-			collisions.Add(direction);
-			world.Destroy(entity);
-			events.Emit(new BlockHitEvent(direction));
-		});
-
-		if (collisions.Count > 0)
-		{
-			if (collisions.Count > 1) Console.WriteLine("Multiple collisions: " + string.Join(", ", collisions));
-
-			var direction = collisions.Select(collisions => collisions switch
-			{
-				CollisionDirection.Top or CollisionDirection.Bottom => new Vector2(1, -1),
-				CollisionDirection.Left or CollisionDirection.Right => new Vector2(-1, 1),
-				_ => Vector2.Zero
-			}).Aggregate(Vector2.One , (totalDir, dir) => totalDir * dir);
-
-			ballVelocity.Direction *= direction;
-		}
-	}
-
-	private CollisionDirection _getIntersectionDirection(ref RectangleF intersection, ref RectangleF ballRect)
-	{
-		if (intersection.Width > intersection.Height) {
-			if (intersection.Top == ballRect.Top) return CollisionDirection.Top;
-			if (intersection.Bottom == ballRect.Bottom) return CollisionDirection.Bottom;
-		} else {
-			if (intersection.Left == ballRect.Left) return CollisionDirection.Left;
-			if (intersection.Right == ballRect.Right) return CollisionDirection.Right;
-		}	
-		
-		return CollisionDirection.Unkown;
-	}
-
-	private Vector2 _recalculateBallDirection(Velocity ballVelocity, CollisionDirection collisionDirection)
-	{
-		return collisionDirection switch
-		{
-			CollisionDirection.Top or CollisionDirection.Bottom => new Vector2(ballVelocity.Direction.X, -ballVelocity.Direction.Y),
-			CollisionDirection.Left or CollisionDirection.Right => new Vector2(-ballVelocity.Direction.X, ballVelocity.Direction.Y),
-			_ => ballVelocity.Direction,
-		};
+		return world.Create(new Ball(), new Position(position), new Sprite(_ballTexture, BreakoutConstants.BALL_SIZE));		
 	}
 }
 
 
-public class BlockSystem(IEventListener events, IAssetManager assets, World world)
+public unsafe class BlockSystem(IEventListener events, IAssetManager assets, World world, PhysicsScene physicsScene)
 {
 	private readonly QueryDescription _blockQuery = new QueryDescription().WithAll<GridLocation, Sprite, Position>();
-	private readonly QueryDescription _ballQuery = new QueryDescription().WithAll<Ball, Velocity>();
 	private readonly QueryDescription _paddleQuery = new QueryDescription().WithAll<Paddle>();
-	private EntityReference _paddle = default!;
-	private EntityReference _ball = default!;
+	private EntityReference _paddle = EntityReference.Null;
+
+	private PxMaterial* _blockMaterial = default!;
 
 	[Init]
 	public void SetupBlocks(GameTime dt, GameLoopDelegate next)
 	{
+		_blockMaterial = physicsScene.CreateMaterial(0.5f, 0.5f, 1f);
+
 		_resetBlocks();
 
 		next(dt);
-
-		world.Query(in _ballQuery, (Entity entity) => {
-			_ball = entity.Reference();
-		});
 
 		world.Query(in _paddleQuery, (Entity entity) => {
 			_paddle = entity.Reference();
@@ -428,11 +374,8 @@ public class BlockSystem(IEventListener events, IAssetManager assets, World worl
 		if (events.On<BlocksClearedEvent>())
 		{
 			ref var paddle = ref _paddle.Entity.Get<Paddle>();
-			ref var ballVelocity = ref _ball.Entity.Get<Velocity>();
 
 			paddle.HasBall = true;
-			ballVelocity.Direction = Vector2.Zero;
-			ballVelocity.Speed = BreakoutConstants.INITIAL_BALL_SPEED;
 
 			Console.WriteLine("You Win!");
 
@@ -451,19 +394,25 @@ public class BlockSystem(IEventListener events, IAssetManager assets, World worl
 		}
 	}
 
-	private void _resetBlocks()
+	private unsafe void _resetBlocks()
 	{
 		var blockTexture = assets.Load<Texture2D>("15-Breakout-Tiles.png");
 
-		var edgeOffset = new Vector2(BreakoutConstants.BLOCK_GAP);
+		var blockHalfExtent = new Vector3(BreakoutConstants.BLOCK_SIZE.X / 2f, BreakoutConstants.BLOCK_SIZE.Y / 2f, 5);
 
 		// Setup blocks in rows and columns across the window each with different colors:
 		for (int row = 0; row < BreakoutConstants.ROWS; row++)
 		{
+			var rowOffset = BreakoutConstants.BLOCK_GAP + blockHalfExtent.Y + (row * (BreakoutConstants.BLOCK_SIZE.Y + BreakoutConstants.BLOCK_GAP));
 			for (int col = 0; col < BreakoutConstants.COLS; col++)
 			{
-				var position = new Position(edgeOffset + new Vector2(col * (BreakoutConstants.BLOCK_SIZE.X + BreakoutConstants.BLOCK_GAP), row * (BreakoutConstants.BLOCK_SIZE.Y + BreakoutConstants.BLOCK_GAP)));
-				world.Create(position, new GridLocation(row, col), new Sprite(blockTexture, BreakoutConstants.BLOCK_SIZE));
+				var colOffset = BreakoutConstants.BLOCK_GAP + blockHalfExtent.X + (col * (BreakoutConstants.BLOCK_SIZE.X + BreakoutConstants.BLOCK_GAP));
+
+				var position = new Position(new Vector2(colOffset, rowOffset));
+				var position3d = new Vector3(position.Value, 0);
+				var body = physicsScene.AddStaticBox(blockHalfExtent, position3d, Quaternion.Identity, _blockMaterial);
+				
+				var blockEntity = world.Create(position, new GridLocation(row, col), new Sprite(blockTexture, BreakoutConstants.BLOCK_SIZE), new StaticBody(body));
 			}
 		}
 	}
